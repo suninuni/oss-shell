8b1753f66a3bbdaa79778a44edc04bdc1c09ac26
Zack Sun <zacksun@augmentum.com.cn>
2017-01-22 17:48:12 +0800

    update

--- oss-tool.js
+++ oss-tool.js
@@ -10,113 +10,308 @@ var OSS = require('ali-oss');
 var co = require('co');
 var env = require('node-env-file');
 var _ = require('lodash');
+var log4js = require('log4js');
 
 // variable
 var ossConf = {
   accessKeyId: null,
   accessKeySecret: null,
-  region: null,
-  bucket: null
+  endpoint: 'http://oss-cn-hangzhou.aliyuncs.com',
+  bucket: null,
+
 }
 var confFile = 'env.conf'
 var client;
+
+log4js.configure('log.json');
+var logger = log4js.getLogger('oss');
+logger.debug(_.join(process.argv, ' '))
 
 // program
 program.usage('[command] [options]')
 
 program
-  .command('create-bucket [bucket]')
+  .command('create-bucket <bucket>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
   .option('-c, --conf <confFile>')
   .action(function(bucket, options) {
-    checkConf(options.conf);
+    checkConf(options);
     if (!bucket) {
-      console.log('bucket is null')
+      logger.info('bucket is null')
       process.exit(1)
     }
     co(function*() {
       var result = yield client.putBucket(bucket);
-      console.log(result.res.status);
+      logger.info(result.res.status);
     }).catch(function(err) {
-      console.log(err);
+      logger.error(err);
       process.exit(1);
     });
   })
 
 program
-  .command('delete-bucket [bucket]')
+  .command('delete-bucket <bucket>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
   .option('-c, --conf <confFile>')
   .action(function(bucket, options) {
-    checkConf(options.conf);
+    checkConf(options);
     if (!bucket) {
-      console.log('bucket is null')
+      logger.info('bucket is null')
       process.exit(1)
     }
     co(function*() {
       var result = yield client.deleteBucket(bucket);
-      console.log(result.res.status);
+      logger.info(result.res.status);
     }).catch(function(err) {
-      console.log(err);
+      logger.error(err);
       process.exit(1);
     });
   })
 
 program
   .command('list-bucket')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
   .option('-c, --conf <confFile>')
   .action(function(options) {
-    checkConf(options.conf);
+    checkConf(options);
     co(function*() {
       let buckets = yield client.listBuckets();
-      console.log(buckets.buckets);
+      logger.info(buckets.buckets);
     }).catch(function(err) {
-      console.log(err);
+      logger.error(err);
+      process.exit(1);
+    })
+  })
+
+
+program
+  .command('upload-files <files...>')
+  .option('-p, --prefix <prefix>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
+  .option('-c, --conf <confFile>')
+  .action(function(files, options) {
+    checkConf(options);
+    let prefix = '';
+    if (options.prefix) {
+      prefix = options.prefix;
+      if (prefix.lastIndexOf('/') != prefix.length - 1) {
+        prefix += '/';
+      }
+    }
+    _.forEach(files, function(file) {
+      uploadFileOrFloder(file, prefix)
+    })
+  })
+
+program
+  .command('download-files <files...>')
+  .option('-p, --prefix <prefix>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
+  .option('-c, --conf <confFile>')
+  .action(function(files, options) {
+    checkConf(options);
+    let prefix = '';
+    if (options.prefix) {
+      prefix = options.prefix;
+      if (prefix.lastIndexOf('/') != prefix.length - 1) {
+        prefix += '/';
+      }
+    }
+    _.forEach(files, function(file) {
+      downloadFile(prefix + file)
+    })
+  })
+
+program
+  .command('download-floders <floders...>')
+  .option('-p, --prefix <prefix>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
+  .option('-c, --conf <confFile>')
+  .action(function(floders, options) {
+    checkConf(options);
+    let prefix = '';
+    if (options.prefix) {
+      prefix = options.prefix;
+      if (prefix.lastIndexOf('/') != prefix.length - 1) {
+        prefix += '/';
+      }
+    }
+    _.forEach(floders, function(floder) {
+      downloadFloder(prefix + floder)
+    })
+  })
+
+
+program
+  .command('delete-files <files...>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
+  .option('-c, --conf <confFile>')
+  .action(function(files, options) {
+    checkConf(options);
+    co(function*() {
+      let result = yield client.deleteMulti(files);
+      logger.info(result);
+    }).catch(function(err) {
+      logger.error(err);
       process.exit(1);
     })
   })
 
 program
-  .command('upload-files [files...]')
+  .command('delete-floders <floders...>')
+  .option('-p, --prefix <prefix>')
+  .option('-n, --number <number>', 'number of files to keep')
+  .option('-e, --expireDate <expireDate>', 'delte file after this expireDate')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
+  .option('-c, --conf <confFile>')
+  .action(function(floders, options) {
+    checkConf(options);
+    let prefix = '';
+    if (options.prefix) {
+      prefix = options.prefix;
+      if (prefix.lastIndexOf('/') != prefix.length - 1) {
+        prefix += '/';
+      }
+    }
+    _.forEach(floders, function(floder) {
+      if (floder.lastIndexOf('/') != floder.length - 1) {
+        floder += '/';
+      }
+      if (options.expireDate) {
+        deleteExpireFile(prefix + floder, options.expireDate);
+      } else if (options.number) {
+        deleteRedundancyFile(prefix + floder, options.number);
+      } else {
+        deleteFloder(prefix + floder)
+      }
+    })
+  })
+
+program
+  .command('copy-file <toAndFrom...>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
   .option('-c, --conf <confFile>')
   .action(function(files, options) {
-    checkConf(options.conf);
-    if (!files) {
-      console.log('files is null')
-      process.exit(1)
+    checkConf(options);
+    if (files.length != 2) {
+      logger.error("you need provide two object paths");
+      process.exit(1);
     }
-    _.forEach(files, function(file) {
+    co(function*() {
+      let result = yield client.copy(files[1], files[0]);
+      logger.info(result);
+    }).catch(function(err) {
+      logger.error(err);
+      process.exit(1);
+    })
+  })
+
+program
+  .command('create-lifecycle')
+  .option('-i, --id <id>')
+  .option('-p, --prefix <prefix>')
+  .option('-u, --status <status>')
+  .option('-d, --days <days>')
+  .option('-t, --date <date>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
+  .option('-c, --conf <confFile>')
+  .action(function(options) {
+    checkConf(options);
+    let newRule = {
+      id: options.id,
+      prefix: options.prefix,
+      status: options.status,
+      days: options.days,
+      date: options.date
+    }
+    co(function*() {
+      let getResult = yield client.getBucketLifecycle(ossConf.bucket, ossConf.endpoint);
+      let rules = getResult.rules;
+      rules.push(newRule);
+      let putResult = yield client.putBucketLifecycle(ossConf.bucket, ossConf.endpoint, rules);
+      logger.info(putResult);
+    }).catch(function(err) {
+      logger.error(err);
+      let rules = [];
+      rules.push(newRule);
       co(function*() {
-        let result;
-        if (fs.statSync(file).size > 100000) {
-          result = yield client.multipartUpload(file, file, {
-            progress: function*(p) {
-              console.log('Progress: ' + p);
-            }
-          });
-        } else {
-          result = yield client.put(file, file);
-        }
-        console.log(result);
-      }).catch(function(err) {
-        console.log(err);
-        process.exit(1);
+        let putResult = yield client.putBucketLifecycle(ossConf.bucket, ossConf.endpoint, rules);
+        logger.info(putResult);
       })
     })
   })
 
 program
-  .command('delete-files [files...]')
+  .command('list-lifecycle')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
   .option('-c, --conf <confFile>')
-  .action(function(files, options) {
-    checkConf(options.conf);
-    if (!files) {
-      console.log('files is null')
-      process.exit(1)
-    }
+  .action(function(options) {
+    checkConf(options);
     co(function*() {
-      let result = yield client.deleteMulti(files);
-      console.log(result);
+      let result = yield client.getBucketLifecycle(ossConf.bucket, ossConf.endpoint);
+      logger.info(result);
     }).catch(function(err) {
-      console.log(err);
+      logger.error(err);
+      process.exit(1);
+    })
+  })
+
+program
+  .command('delete-lifecycle')
+  .option('-i, --id <id>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
+  .option('-c, --conf <confFile>')
+  .action(function(options) {
+    checkConf(options);
+    co(function*() {
+      let getResult = yield client.getBucketLifecycle(ossConf.bucket, ossConf.endpoint);
+      let rules = getResult.rules;
+      let index = _.findIndex(rules, function(rule) {
+        return rule.id == options.id;
+      })
+      if (index >= 0) {
+        _.pullAt(rules, index);
+      }
+      let putResult = yield client.putBucketLifecycle(ossConf.bucket, ossConf.endpoint, rules);
+      logger.info(putResult);
+    }).catch(function(err) {
+      logger.error(err);
       process.exit(1);
     })
   })
@@ -125,17 +320,23 @@ program
   .command('list-files')
   .option('-m, --marker <marker>')
   .option('-p, --prefix <prefix>')
+  .option('-d, --delimiter <delimiter>')
+  .option('-k, --accessKeyId <accessKeyId>')
+  .option('-s, --accessKeySecret <accessKeySecret>')
+  .option('-e, --endpoint <endpoint>')
+  .option('-b, --bucket <bucket>')
   .option('-c, --conf <confFile>')
   .action(function(options) {
-    checkConf(options.conf);
+    checkConf(options);
     co(function*() {
       let files = yield client.list({
         marker: options.marker,
-        prefix: options.prefix
+        prefix: options.prefix,
+        delimiter: options.delimiter
       });
-      console.log(files.objects);
+      logger.info(files.objects);
     }).catch(function(err) {
-      console.log(err);
+      logger.error(err);
       process.exit(1);
     })
   })
@@ -144,25 +345,11 @@ program
   .command('conf')
   .option('-k, --accessKeyId <accessKeyId>')
   .option('-s, --accessKeySecret <accessKeySecret>')
-  .option('-r, --region <region>')
+  .option('-e, --endpoint <endpoint>')
   .option('-b, --bucket <bucket>')
-  .option('-f, --file <filePath>', 'conf file, which need set the accessKeyId, accessKeySecret and region', 'dev.conf')
+  .option('-c, --conf <filePath>', 'conf file, which need set the accessKeyId, accessKeySecret and endpoint', 'dev.conf')
   .action(function(options) {
-    if (options.file) {
-      setOssConfFromFile(options.file)
-    }
-    if (options.accessKeyId) {
-      ossConf.accessKeyId = options.accessKeyId;
-    }
-    if (options.accessKeySecret) {
-      ossConf.accessKeySecret = options.accessKeySecret;
-    }
-    if (options.region) {
-      ossConf.region = options.region;
-    }
-    if (options.region) {
-      ossConf.bucket = options.bucket;
-    }
+    setOssConfFromArgs(options)
     writeOssConfToFile();
   })
 
@@ -170,23 +357,205 @@ program.parse(process.argv);
 
 //function
 
-function getOss() {
-  if (arguments.length == 1) {
-    confFile = arguments[0];
+function uploadFileOrFloder(file, prefix) {
+  if (fs.statSync(file).isFile()) {
+    uploadFile(file, prefix);
+  } else if (fs.statSync(file).isDirectory) {
+    prefix += file.substr(file.lastIndexOf('/') + 1) + '/';
+    uploadFloder(file, prefix);
+  } else {
+    logger.info("Can't find file or floder: " + file);
   }
-  setOssConfFromFile(confFile);
-  return new OSS(ossConf);
+}
+
+function uploadFloder(floder, prefix) {
+  let items = fs.readdirSync(floder);
+  _.forEach(items, function(item) {
+    item = floder + '/' + item;
+    uploadFileOrFloder(item, prefix)
+  })
+}
+
+function uploadFile(file, prefix) {
+  co(function*() {
+    let result;
+    let fileName = file.substr(file.lastIndexOf('/') + 1);
+    if (fs.statSync(file).size > 4194304) {
+      let checkpoint;
+      for (var i = 0; i < 5; i++) {
+        logger.info("multi part upload file " + prefix + fileName);
+        result = yield client.multipartUpload(prefix + fileName, file, {
+          checkpoint: checkpoint,
+          partSize: 10000 * 1024,
+          progress: function*(percentage, cpt) {
+            checkpoint = cpt;
+          }
+        });
+        logger.info(result);
+        break; // break if success
+      }
+    } else {
+      logger.info("upload file: " + prefix + fileName);
+      logger.info(prefix + fileName);
+      result = yield client.put(prefix + fileName, file);
+      logger.info(result);
+    }
+  }).catch(function(err) {
+    logger.error(err);
+    process.exit(1);
+  })
+}
+
+function downloadFloder(floder) {
+  co(function*() {
+    let toDownloadFiles = [];
+    if (floder.lastIndexOf('/') != floder.length - 1) {
+      floder += '/';
+    }
+    let searchResult = yield client.list({
+      prefix: floder
+    });
+    _.forEach(searchResult.objects, function(obj) {
+      toDownloadFiles.push(obj.name);
+    })
+    _.forEach(toDownloadFiles, function(file) {
+      downloadFile(file, '');
+    })
+  }).catch(function(err) {
+    logger.error(err);
+    process.exit(1);
+  })
+}
+
+function downloadFile(file) {
+  co(function*() {
+    mkdirsSync(file.substr(0, file.lastIndexOf('/')))
+    let result = yield client.get(file, file);
+    logger.info(result);
+  }).catch(function(err) {
+    logger.error(err);
+    process.exit(1);
+  })
+}
+
+function mkdirsSync(dirpath) {
+  if (!fs.existsSync(dirpath)) {
+    let pathtmp;
+    dirpath.split('/').forEach(function(dirname) {
+      if (pathtmp) {
+        pathtmp = pathtmp + '/' + dirname;
+      } else {
+        pathtmp = dirname;
+      }
+      if (!fs.existsSync(pathtmp)) {
+        if (!fs.mkdirSync(pathtmp)) {
+          return false;
+        }
+      }
+    });
+  }
+  return true;
+}
+
+function deleteFloder(floder) {
+  co(function*() {
+    let toDeleteFiles = []
+    let searchResult = yield client.list({
+      prefix: floder
+    });
+    _.forEach(searchResult.objects, function(obj) {
+      toDeleteFiles.push(obj.name)
+    })
+    let result = yield client.deleteMulti(toDeleteFiles);
+    logger.info(result);
+  }).catch(function(err) {
+    logger.error(err);
+    process.exit(1);
+  })
+}
+
+function deleteExpireFile(floder, expireDate) {
+  co(function*() {
+    let toDeleteFiles = []
+    let searchResult = yield client.list({
+      prefix: floder
+    });
+    let sortedObjects = _.sortBy(searchResult.objects, function(object) {
+      return object.lastModified;
+    })
+
+    let toDelteObjects = _.remove(sortedObjects, function(object) {
+      return object.lastModified < expireDate;
+    })
+
+    _.forEach(toDelteObjects, function(obj) {
+      toDeleteFiles.push(obj.name)
+    })
+    let result = yield client.deleteMulti(toDeleteFiles);
+    logger.info(result);
+  }).catch(function(err) {
+    logger.error(err);
+    process.exit(1);
+  })
+}
+
+function deleteRedundancyFile(floder, number) {
+  co(function*() {
+    let toDeleteFiles = []
+    let searchResult = yield client.list({
+      prefix: floder
+    });
+    if (searchResult.objects.length > number) {
+      let sortedObjects = _.sortBy(searchResult.objects, function(object) {
+        return object.lastModified;
+      })
+      sortedObjects = _.dropRight(sortedObjects, number);
+
+      _.forEach(sortedObjects, function(obj) {
+        toDeleteFiles.push(obj.name)
+      })
+
+      let result = yield client.deleteMulti(toDeleteFiles);
+      logger.info(result);
+    }
+  }).catch(function(err) {
+    logger.error(err);
+    process.exit(1);
+  })
+}
+
+function setOssConfFromArgs(options) {
+  if (options.conf) {
+    setOssConfFromFile(options.conf)
+  } else {
+    setOssConfFromFile(confFile)
+  }
+  if (options.accessKeyId) {
+    ossConf.accessKeyId = options.accessKeyId;
+  }
+  if (options.accessKeySecret) {
+    ossConf.accessKeySecret = options.accessKeySecret;
+  }
+  if (options.endpoint) {
+    ossConf.endpoint = options.endpoint;
+  }
+  if (options.bucket) {
+    ossConf.bucket = options.bucket;
+  }
 }
 
 function setOssConfFromFile(file) {
   if (!fs.existsSync(file)) {
-    console.log('conf file ' + file + ' is not exist, you need create it or run os-tool conf first')
+    if (file == confFile) {
+      return;
+    }
+    logger.info('conf file ' + file + ' is not exist, you need create it or run os-tool conf first')
     process.exit(1);
   } else {
     env(file);
     ossConf.accessKeyId = process.env.accessKeyId;
     ossConf.accessKeySecret = process.env.accessKeySecret;
-    ossConf.region = process.env.region;
+    ossConf.endpoint = process.env.endpoint;
     ossConf.bucket = process.env.bucket;
   }
 }
@@ -198,10 +567,7 @@ function writeOssConfToFile() {
   })
 }
 
-function checkConf(file) {
-  if (file) {
-    client = getOss(file)
-  } else {
-    client = getOss()
-  }
+function checkConf(options) {
+  setOssConfFromArgs(options);
+  client = new OSS(ossConf);
 }
